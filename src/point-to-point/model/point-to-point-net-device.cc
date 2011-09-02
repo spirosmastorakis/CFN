c996055dcfcd8464008611fdb08ee622074a2710
Ilya Moiseenko <iliamo@ucla.edu>
2011-09-01 17:41:00 -0700

    point-to-point: Add support of NDN protocol
    - Add support of PPP->Ethernet and Ethernet->PPP frame type conversion
      for NDN protocol
    - Adding support for NDN payload in PppHeader::Print method

--- src/point-to-point/model/point-to-point-net-device.cc
+++ src/point-to-point/model/point-to-point-net-device.cc
@@ -689,6 +689,7 @@ PointToPointNetDevice::PppToEther (uint16_t proto)
     {
     case 0x0021: return 0x0800;   //IPv4
     case 0x0057: return 0x86DD;   //IPv6
+    case 0x0077: return 0x7777;   //NDN
     default: NS_ASSERT_MSG (false, "PPP Protocol number not defined!");
     }
   return 0;
@@ -702,6 +703,7 @@ PointToPointNetDevice::EtherToPpp (uint16_t proto)
     {
     case 0x0800: return 0x0021;   //IPv4
     case 0x86DD: return 0x0057;   //IPv6
+    case 0x7777: return 0x0077;   //NDN
     default: NS_ASSERT_MSG (false, "PPP Protocol number not defined!");
     }
   return 0;
--- src/point-to-point/model/ppp-header.cc
+++ src/point-to-point/model/ppp-header.cc
@@ -67,6 +67,9 @@ PppHeader::Print (std::ostream &os) const
     case 0x0057: /* IPv6 */
       proto = "IPv6 (0x0057)";
       break;
+    case 0x0077: /* NDN */
+      proto = "NDN (0x0077)";
+      break;
     default:
       NS_ASSERT_MSG (false, "PPP Protocol number not defined!");
     }
