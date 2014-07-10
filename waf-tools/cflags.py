76aa766d2a926549bacf82fb9b51f6a451e79f62
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2014-07-09 21:08:50 -0700

    build: Disable -Werror flags

--- waf-tools/cflags.py
+++ waf-tools/cflags.py
@@ -19,7 +19,7 @@ class GccTraits(CompilerTraits):
 	def __init__(self):
 		super(GccTraits, self).__init__()
 		# cumulative list of warnings per level
-		self.warnings_flags = [['-Wall'], ['-Werror'], ['-Wextra']]
+		self.warnings_flags = [['-Wall']]
 
 	def get_warnings_flags(self, level):
 		warnings = []
