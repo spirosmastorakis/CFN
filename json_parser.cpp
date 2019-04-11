cabedbc4e825fed962507dded7468113741bfe1e
Michał Król <mharnen@gmail.com>
2019-04-11 15:04:36 -0700

    json parser

--- json_parser.cpp
+++ json_parser.cpp
@@ -3,20 +3,10 @@
 #include "boost/property_tree/ptree.hpp"
 #include "boost/property_tree/json_parser.hpp"
 
-template <typename T>
-std::vector<T> as_vector(boost::property_tree::ptree const& pt, boost::property_tree::ptree::key_type const& key)
-{
-    std::vector<T> r;
-    for (auto& item : pt.get_child(key))
-        r.push_back(item.second.get_value<T>());
-    return r;
-}
-
 int main() {
     using boost::property_tree::ptree;
 
     std::ifstream jsonFile("test.json");
-    //std::ifstream jsonFile("input.txt");
 
     ptree pt;
     read_json(jsonFile, pt);
