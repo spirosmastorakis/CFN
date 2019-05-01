a5c98c2ea8c5e1fa9fca65b14c143903a4957a0c
Michał Król <mharnen@gmail.com>
2019-05-01 17:39:35 +0100

    inputs

--- generator.py
+++ generator.py
@@ -59,3 +59,23 @@ for i in range(1, 10):
     update_list.append(update)
 
 json.dump(update_list, open("simple_data_10.json", "w"))
+
+
+update_list = list()
+for i in range(1, 1000):
+    update = {}
+    update['name'] = "/exec/f"+str(i)
+    update['type'] = 0
+    update['inputs'] = list()
+    if(i > 1):
+        update['inputs'].append("d"+str(i-1) + ":44")
+    update['outputs'] = list()
+    update['outputs'].append("d"+str(i) + ":44")
+    update['thunk'] = "dupa"
+    update['start'] = 0
+    update['duration'] = 10
+    print(update['name'])
+
+    update_list.append(update)
+
+json.dump(update_list, open("simple_data_1000.json", "w"))
