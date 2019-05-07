e83d13b1a8fce0d7840f0708effe4b894da4b1f9
Michał Król <mharnen@gmail.com>
2019-05-07 17:27:58 +0100

    new inputs

--- generator.py
+++ generator.py
@@ -15,58 +15,65 @@ def generateRandomTree(nodes):
     #convert the graph to a tree (unidirectional edges)
     #return nx.bfs_tree(G,0).to_directed() # already directed, returns deep copy
 
+tree_size = 1000
+outputs_size = 10
+for data_size in range(10, 65, 5):
+    for inputs_num in range (0, 21):
+        for topo in range(1, 2):
+            G = generateRandomTree(tree_size)
 
-for topo in range(5, 50):
-    G = generateRandomTree(1000)
+            print("Our roots:", [n for n,d in G.in_degree() if d==0] )
+            print("Our leafs:", [n for n,d in G.out_degree() if d==0] )
 
-    print("Our roots:", [n for n,d in G.in_degree() if d==0] )
-    print("Our leafs:", [n for n,d in G.out_degree() if d==0] )
+            counter = 0
+            topo_sorted = list(nx.topological_sort(G))
+            update_list = list()
 
-    counter = 0
-    topo_sorted = list(nx.topological_sort(G))
-    update_list = list()
+            for node in  topo_sorted:
+                update = {}
+                outputs = random.randrange(0, outputs_size)
 
-    for node in  topo_sorted:
-        update = {}
-        outputs = random.randrange(0, 3)
+                update['name'] = "/exec/f"+str(node)
+                update['type'] = 0
 
-        update['name'] = "/exec/f"+str(node)
-        update['type'] = 0
+                update['caller'] = ""
+                preds = G.predecessors(node)
+                for pred in preds:
+                    print("pred", pred)
+                    update['caller'] = "/exec/f" + str(pred)
 
-        update['caller'] = ""
-        preds = G.predecessors(node)
-        for pred in preds:
-            print("pred", pred)
-            update['caller'] = "/exec/f" + str(pred)
+                update['inputs'] = list()
+                update['outputs'] = list()
+                for i in range(0, outputs):
+                    size = random.randrange(1, data_size)
+                    update['outputs'].append(update['name'] + "/d" + str(i) + ":" + str(size))
+                    #outputs = random.randrange(0, 3)
+                update['thunk'] = ""
+                update['start'] = counter
+                update['duration'] =  random.randrange(1, 30)
 
-        update['inputs'] = list()
-        update['outputs'] = list()
-        for i in range(0, outputs):
-            size = random.randrange(1, 50)
-            update['outputs'].append(update['name'] + "/d" + str(i) + ":" + str(size))
-            #outputs = random.randrange(0, 3)
-        update['thunk'] = ""
-        update['start'] = counter
-        update['duration'] =  random.randrange(1, 30)
+                counter += 1
+                update_list.append(update)
 
-        counter += 1
-        update_list.append(update)
+            for index in range(0, len(update_list)):
+                update = update_list[index]
+                for nested_index in range(index+1, len(update_list)):
 
-    for index in range(0, len(update_list)):
-        update = update_list[index]
-        for nested_index in range(index+1, len(update_list)):
+                    nested_update = update_list[nested_index]
+                    #print("Nested name:", nested_update['name'], "len:", len(nested_update['inputs']), end='')
+                    #if(len(nested_update['inputs']) < inputs_num):
+                        #print("in")
+                    for output in update['outputs']:
+                        if(random.random() > 0.7 and len(nested_update['inputs']) < inputs_num):
+                            nested_update['inputs'].append(output)
 
-            nested_update = update_list[nested_index]
-            if(len(nested_update['inputs']) > 5):
-                continue
-            for output in update['outputs']:
-                if(random.random() > 0.8):
-                    nested_update['inputs'].append(output)
+                    #else:
+                        #print("out")
 
-    for update in update_list:
-        print(update)
+            for update in update_list:
+                print(update)
 
-    json.dump(update_list, open("locality"+str(topo) + ".json", "w"))
+            json.dump(update_list, open("locality"+"_inputs_" + str(inputs_num) + "_datasize_" + str(data_size) +"_" + str(topo) + ".json", "w"))
 #nx.draw_networkx(G)
 #plt.show()
 
